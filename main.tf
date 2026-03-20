6b75311ce227beb0eaa15075eb0a04cfb310ce45
Anmol Katiyar <84003752+anmolk03@users.noreply.github.com>
2026-03-21 01:00:05 +0530

    Add comments for Azure CLI and Terraform installation
    Add installation instructions for Azure CLI and Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+##sudo ./svc.sh install
+##sudo ./svc.sh start
+
+###curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+##wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list && sudo apt-get update && sudo apt-get install terraform -y
+##sudo apt-get update && sudo apt-get install -y git unzip jq
+
 ##Create storage for state via portal
 
 terraform {
